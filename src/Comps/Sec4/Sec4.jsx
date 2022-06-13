ab424b1ad7eb52252f1204cd6581e8b09f196592
Bhavik Prajapati <rahulprajapati1665@gmail.com>
2022-06-13 15:03:42 +0530

    done

--- src/Comps/Sec4/Sec4.jsx
+++ src/Comps/Sec4/Sec4.jsx
@@ -7,8 +7,8 @@ const Sec4 = () => {
       <MyLine />
       <h2>About Me</h2>
       <p style={{ marginBottom: "70px" }}>
-        The purpose of Mine is to to take My development skills to the next
-        level and build awesome apps.
+        The purpose of Mine is to take My development skills to the next level
+        and build awesome apps.
       </p>
       <div className="div1">
         <a href="mailto:bhavikprajapati4244@gmail.com">Mail me</a>
