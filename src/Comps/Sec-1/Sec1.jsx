4ca2ec137581bb5ee72c6a3da5740886be63df51
Bhavik Prajapati <69726429+rahul429-wq@users.noreply.github.com>
2023-06-18 09:51:53 +0530

    Update Sec1.jsx

--- src/Comps/Sec-1/Sec1.jsx
+++ src/Comps/Sec-1/Sec1.jsx
@@ -15,7 +15,7 @@ const Sec1 = () => {
         <a
           target="_blank"
           className="my-btn"
-          href="https://drive.google.com/file/d/1VenLKEQFCYAsVmIUQLNYijTDXoie_0jv/view?usp=sharing"
+          href="https://drive.google.com/file/d/1ABwlnhvqIDYto1loB-7s9hrEwR3WbM8X/view?usp=sharing"
         >
           Resume
         </a>
